bd1ae9bd3fb822664cceca5eefa3a876bc23f0df
David Liao <liao.canada@gmail.com>
2020-05-21 15:51:24 -0400

    Ignore temp Excel lock files

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const xlsxToMarkdown = require('./xlsxToMarkdown');
 
-
 fs.readdirSync(path.join(__dirname, '../spreadsheets/')).forEach(filename => {
+    // Ignore temp Excel lock files
+    if (filename.startsWith('~$')) return;
+
     const extensionRemoved = filename.split('.')[0];
     console.log(`Converting ${extensionRemoved}.xlsx to markdown table --> /tables/${extensionRemoved}.md...`);
     xlsxToMarkdown(extensionRemoved);
